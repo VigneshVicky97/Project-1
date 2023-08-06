d08258435d52b0b345fb9aacd46f0eec17c3b150
Vignesh S <vigneshkavery97@gmail.com>
2023-08-06 23:22:39 +0530

    Include PostgreSQL commands and Environment keys

--- main.py
+++ main.py
@@ -263,7 +263,7 @@ def about():
 def contact():
     return render_template("contact.html", current_user=current_user)
 
-# Optional: You can inclue the email sending code from Day 60:
+# Optional: You can include the email sending code from Day 60:
 # DON'T put your email and password here directly! The code will be visible when you upload to Github.
 # Use environment variables instead (Day 35)
 
