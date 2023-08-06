1c87590828c936e0222e6f7a025ddf1afdba090a
Vignesh S <vigneshkavery97@gmail.com>
2023-08-07 02:48:13 +0530

    Trying to remove error in Contact function

--- main.py
+++ main.py
@@ -286,12 +286,12 @@ def send_email(name, email, phone, message):
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MAIL_ADDRESS, MAIL_APP_PW)
-        connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
-        # connection.sendmail(
-        #     from_addr=MAIL_ADDRESS,
-        #     to_addrs=TO_MAIL_ADDRESS,
-        #     msg=email_message
-        # )
+        # connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
+        connection.sendmail(
+            from_addr=MAIL_ADDRESS,
+            to_addrs=TO_MAIL_ADDRESS,
+            msg=email_message
+        )
 
 
 if __name__ == "__main__":
